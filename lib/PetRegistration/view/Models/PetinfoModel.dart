adf9b1e0ecef1693fb46bfd8dd9c336e9f13b32a
tejayarlagadda1996 <i.tejaa@gmail.com>
2024-06-10 10:23:15 +0530

    Updated code

--- lib/PetRegistration/view/Models/PetinfoModel.dart
+++ lib/PetRegistration/view/Models/PetinfoModel.dart
@@ -1,17 +1,9 @@
 class Petinfomodel{
   final String petname;
   final double petage;
-  final Pettype pettype;
-  final Petgender petgender;
 
   const Petinfomodel({
     required this.petage,
     required this.petname,
-    required this.pettype,
-    required this.petgender,
   });
 }
-
-enum Pettype{dog,cat,bird,other}
-enum Petgender{male,female}
-enum Petbreed{a,b,c,d,e}
