0f9e24ab10559e737d2445fafd4842529529ba9d
tejayarlagadda1996 <i.tejaa@gmail.com>
2024-07-05 18:54:56 +0530

    changes

--- lib/Networkmanager/PetAppRest.dart
+++ lib/Networkmanager/PetAppRest.dart
@@ -38,8 +38,7 @@ class Petapprest<T> {
   Future<T?> post(String endpoint, Map<String, dynamic> body, T Function(dynamic) fromJson, Authentication? auth, Function(int) setStatusCode) async {
     var basicAuth = auth != null ? 'Basic ${base64Encode(utf8.encode('rzp_test_4wZO5YRyjtACT2:nvGyVuemI5s643oUVGyzpsex'))}' : '' ;
     var url = Uri.parse('$baseUrl$endpoint');
-    var headers =
-    auth != null ? {'Content-Type': 'application/json','Authorization': basicAuth} : {'Content-Type': 'application/json', 'accept': 'application/json'};
+    var headers = auth != null ? {'Content-Type': 'application/json','Authorization': basicAuth} : {'Content-Type': 'application/json', 'accept': 'application/json'};
     var jsonBody = jsonEncode(body);
     var response =  await http.post(url, body: jsonBody,headers: headers);
     if (response.statusCode == 200 || response.statusCode == 400 || response.statusCode == 404 || response.statusCode == 201) {
